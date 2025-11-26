c623754dffbd6b56c8ef0268aedb1c6bf9c9c988
hermextravels <praise.ordu@hermextravels.com>
2025-11-26 04:15:26 +0100

    first commit

--- include/gpu_bsgs.h
+++ include/gpu_bsgs.h
@@ -1,11 +1,20 @@
+
 #pragma once
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// BSGS main entry point
-// Returns 1 if key found, 0 otherwise
-int gpu_bsgs_solve(const char* address, const char* start_hex, const char* end_hex, int bits, unsigned char* found_key);
+// Main BSGS solver entry point
+// Returns 0 if key found, -1 otherwise
+int gpu_bsgs_solve(
+	const char* address,
+	const char* start_hex,
+	const char* end_hex,
+	int bits,
+	uint64_t* found_key
+);
 
 #ifdef __cplusplus
 }
